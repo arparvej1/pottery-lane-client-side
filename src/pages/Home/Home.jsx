2312decae2c0bd113f54948980ae4b81043c795c
arparvej1 <parvej8pro@gmail.com>
2024-04-26 20:55:21 +0600

    firebase first deploy

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,19 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
 
 const Home = () => {
-
+  const { apiURL } = useContext(AuthContext);
+  const [users, setUsers] = useState([]);
+  fetch(`${apiURL}/users`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setUsers(data)
+    })
   return (
     <>
       <h3>Hello Client Side</h3>
+      <p>User: {users.length}</p>
     </>
   );
 };
